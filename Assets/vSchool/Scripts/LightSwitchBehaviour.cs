f236d33a00f22aa2329fcb4dd97b92ba3b891bfc
Simon <simon.caignart@gmail.com>
2022-12-09 11:31:19 +0100

    add lightswitch

---
+++ Assets/vSchool/Scripts/LightSwitchBehaviour.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitchBehaviour : MonoBehaviour
+{
+    public List<GameObject> Ligths;
+    public bool isLightOn;
+    private bool playerInZone;
+    private Animator animator;
+    public GameObject textToDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        playerInZone = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInZone && Input.GetKeyDown(KeyCode.E))
+        {
+            isLightOn = !isLightOn;
+            animator.SetBool("isLightOn", isLightOn);
+            foreach (GameObject light in Ligths)
+            {
+                light.SetActive(!light.activeSelf);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            textToDisplay.SetActive(true);
+            playerInZone = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            textToDisplay.SetActive(false);
+            playerInZone = false;
+        }
+    }
+
+}
